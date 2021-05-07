4201837bbd2f6efff41fa12585cfbd9d0112b35e
Axium1998 <4612427+axium1998@users.noreply.github.com>
2021-05-07 01:54:49 -0500

    Start of something new :)

---
+++ MaidMike/CmdModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Interactivity.Extensions;
+
+namespace MaidMike {
+    public class CmdModule : BaseCommandModule {
+        
+        [Command("ping")]
+        public async Task PingCmd(CommandContext ctx) {
+            await ctx.RespondAsync("Pong!");
+        }
+    }
+}
---
+++ MaidMike/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Enums;
+using DSharpPlus.Interactivity.Extensions;
+
+namespace MaidMike {
+    internal static class Program {
+        private static async Task Main() {
+            var discord = new DiscordClient(new DiscordConfiguration() {
+                Token = GetToken(),
+                TokenType = TokenType.Bot
+            });
+
+            discord.UseInteractivity(new InteractivityConfiguration() {
+                PollBehaviour = PollBehaviour.KeepEmojis,
+                Timeout = TimeSpan.FromSeconds(30)
+            });
+
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration() { StringPrefixes = new[] {"!"}});
+            commands.RegisterCommands<CmdModule>();
+            commands.RegisterConverter(new ArgConverter());
+            
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static string GetToken() {
+            var lines = File.ReadAllLines(Directory.GetCurrentDirectory() + "\\config.cfg");
+            return (from x in lines select x.Split("=") into line where line[0].Equals("token") select line[1]).FirstOrDefault();
+        }
+    }
+}
